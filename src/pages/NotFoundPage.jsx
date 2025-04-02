40b95fb5685f5bf24f57f3db5133d067db1d75d4
Omarespo-dev <omaresposito21@gmail.com>
2025-04-02 19:32:39 +0200

    added style page 404

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom"
 
 const NotFoundPage = () => {
     return (
-        <div>
+        <div className="div-notFound">
             <h2>Page Not Found</h2>
             <p>Sorry, but the page you are looking for does not exist</p>
-            <Link className="button" to="/">Back to home</Link>
+            <Link  to="/"> <button className="button">Back to home </button></Link>
         </div>
     )
 }
