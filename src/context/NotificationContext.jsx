9e49fae830dbcb2c09b37127cb5457fa10d7d94b
Omarespo-dev <omaresposito21@gmail.com>
2025-04-02 16:30:12 +0200

    changed color

--- src/context/NotificationContext.jsx
+++ src/context/NotificationContext.jsx
@@ -22,13 +22,13 @@ export const NotificationProvider = ({ children }) => {
                     top: "50%",
                     left: "50%",
                     transform: "translate(-50%, -50%)",
-                    background: notificationType === "error" ? "#FF0000" : "#4caf50",  // Rosso per errore, verde per successo
+                    background: notificationType === "error" ? "#FF0000" : "#0C68F4",  // Rosso per errore, verde per successo
                     color: "white",
                     padding: "10px 15px",
                     borderRadius: "5px",
-                    boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)",
+                    boxShadow: "0px 6px 9px rgba(0, 0, 0, 0.1)",
                     zIndex: 9999,
-                    // transition: 'opacity 0.3s ease-in-out',
+                    transition: 'opacity 0.3s ease-in-out',
                 }}>
                     {notification}
                 </div>
